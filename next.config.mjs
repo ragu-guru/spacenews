1b2a64f195999a39cd263a5319b4743aba9be35a
Ragu <ragug87@gmail.com>
2024-10-24 11:16:07 +0800

    revert next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                // Enable CORS headers for all routes
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET, POST, OPTIONS',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-Requested-With, Content-Type, Authorization',
-                    },
-                ],
-            },
-        ];
-    }
 };
 
 export default nextConfig;
