86e8c4d606126eea1229041f6063155d1d49c221
Ragu <ragug87@gmail.com>
2024-10-24 11:08:25 +0800

    added api congigs

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,24 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                // Enable CORS headers for all routes
+                source: '/(.*)',
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-Requested-With, Content-Type, Authorization',
+                    },
+                ],
+            },
+        ];
     }
 };
 
